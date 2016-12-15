524991204311704add67cfb7ddd59b22d0275934
Joe Offutt <joff@localhost.localdomain>
2016-12-14 21:16:59 -0500

    committing random shit

---
+++ web/app/main.component.ts
@@ -0,0 +1,14 @@
+import {Component, View} from "angular2/core";
+
+@Component({
+   selector: 'my-app'
+})
+
+@View({
+  template: '<div class="jokeHeader"> Welcome to the most Stuperful website ever assembled </div>',
+  styleUrls: ['/styles/main.css']
+})
+
+export class MyHelloWorldClass {
+
+}
---
+++ web/app/main.ts
@@ -0,0 +1,8 @@
+import {bootstrap} from '@angular/platform-browser-dynamic'
+import {Header} from "./header.component"
+import {MyHelloWorldClass} from "./main.component"
+/*import {TabDemo} from "./Tab.Demo"*/
+
+bootstrap(Header);
+/*bootstrap(TabDemo);*/
+bootstrap(MyHelloWorldClass);
