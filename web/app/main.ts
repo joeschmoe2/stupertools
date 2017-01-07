9d9d91403faa8232fc9f6c068a905c16ca405148
Joe Offutt <joff@localhost.localdomain>
2017-01-07 14:36:00 -0500

    cleaning up crap

--- web/app/main.ts
+++
@@ -1,6 +0,0 @@
-import {bootstrap} from '@angular/platform-browser-dynamic';
-import {Header} from './header.component';
-import {MainApp} from './main.component';
-
-bootstrap(Header);
-bootstrap(MainApp);
